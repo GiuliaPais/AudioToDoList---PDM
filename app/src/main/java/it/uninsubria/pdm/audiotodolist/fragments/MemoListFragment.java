dc86a441fb881876664245eacd72a25369ef5299
GiuliaPais <giuliapais1@gmail.com>
2021-01-19 17:45:19 +0100

    [NEW] Added initial structure and set up for different devices

---
+++ app/src/main/java/it/uninsubria/pdm/audiotodolist/fragments/MemoListFragment.java
@@ -0,0 +1,11 @@
+package it.uninsubria.pdm.audiotodolist.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import it.uninsubria.pdm.audiotodolist.R;
+
+public class MemoListFragment extends Fragment {
+    public MemoListFragment() {
+        super(R.layout.recycler_view_fragment);
+    }
+}
